e00d8b09b518b1515a5cedcea9446ee286741dd4
aaron <aaron@aaron-ThinkPad-T430s.(none)>
2013-12-18 18:42:26 -0500

    adding grid.py

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,34 @@
-"""Solution to an exercise in Think Python.
+"""Grid Excersice By Aaron Shapiro"""
 
-Author:
-"""
 
-print '+ - - - -'
+def do_twice(f):
+    f()
+    f()
+
+def do_four(f):
+    do_twice(f)
+    do_twice(f)
+
+def print_hor():
+    print '+ - - - -',
+
+def print_vert():
+    print '|        ',
+
+def print_horz():
+    do_twice(print_hor)
+    print '+'
+
+def print_verts():
+    do_twice(print_verts)
+    print '|'
+
+def print_row():
+    print_horz()
+    do_four(print_horz)
+
+def print_grid():
+    do_twice(print_row)
+    print_verts()
+
+print_grid()
